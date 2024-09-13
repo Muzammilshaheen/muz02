ae57e3d108ec0032e775eda2217c85eba2fceac7
mariaghafoor636 <mariaghafoor636@gmail.com>
2024-09-13 09:53:02 +0500

    muz02

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="text-lg text-4xl bg-pink-200 rounded-full text-xs text-xl ml-auto px-2 py-2 text-cyan-500 italic p-6 gap-6 text-center">
     <Link href="\">home page,</Link>
     <Link href="\about">About,</Link>
-    <Link href="\adress">Adress,</Link>
+    <Link href="\adress">Adress,,</Link>
 
     </div>
      )
